1ebad615d1a34afe96c7511384fec8ac8b9f3085
John Maybee <john.d.maybee@gmail.com>
2022-12-11 00:26:09 -0500

    done with example

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,21 +1,71 @@
 $(readyNow); // does same thing as $(document).ready(readyNow)
 
+let operator;
+
 function readyNow() {
     console.log('jquery ready');
-
+    // trigger a GET 
+    getCalc(); 
 
     $('#submitBtn').on('click', handleSubmit);
     $('.operator').on('click', handleOperator);
 }
 
+function getCalc() {
+    console.log('GET Trigger');
+
+    $.ajax({
+        url: '/calculate',
+        method: 'GET',
+    }).then(function(response) {
+        console.log(response);
+        render(response);
+        // operator = response.operator;
+    })
+}
+
+function render(history) {
+    // render answer
+    $('#answer').empty();
+    $('#answer').append(`<h2>${history[history.length - 1].answer}</h2>`);
+    
+    // render history
+    $('#history').empty();
+    for (let calc of history) {
+        $('#history').append(`
+        <li>
+            ${calc.num1} ${calc.operator} ${calc.num2} = ${calc.answer}
+        </li>`)
+    }
+}
+
 function handleOperator() {
-    console.log('clicked Operator', $(this));
+    console.log('clicked Operator', $(this).text());
+    operator = $(this).text();
 }
 
 function handleSubmit() {
     console.log('clicked!');
     let num1 = $('#num1').val();
     let num2 = $('#num2').val();
-    console.log(num1, num2);
+    console.log(num1, num2, operator);
+
+    // promise (how js handles asyncronous code)
+    $.ajax({
+        url: '/calculate',
+        method: 'POST',
+        data: {
+            num1: num1,
+            num2: num2,
+            operator: operator
+        }
+    }).then(function(response) {
+        console.log(response);
+        // trigger a GET
+        getCalc();
+    }).catch(function(error) {
+        console.log(error);
+        alert(error.responseText);
+    })
 }
 
--- server/server.js
+++ server/server.js
@@ -3,9 +3,61 @@ const app = express();
 const port = 5001; // where we will be loading/accessing our code
 
 // Serve static files
+
+
+// body parser could be used here instead of express?
+app.use(express.urlencoded({ extended: true }));
+
+// Serve Static files
 app.use(express.static('server/public')); // this serves up a static that has the index.html inside.
-app.use(express.urlencoded());
+
+let history = [];
+
+app.get('/calculate', (req, res) => {
+    console.log('Got to GET /calculate');
+    res.send(history);
+})
+
+app.post('/calculate', (req, res) => {
+    console.log('IN POST /calculate', req.body);
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let operator = req.body.operator;
+    let answer;
+
+
+    // do the maths...
+    // if(req.body.operator === '+')
+    switch(operator){
+        case '+':
+            answer = num1 + num2;
+            break;
+        case '-':
+            answer = num1 - num2;
+            break;
+        case '*':
+            answer = num1 * num2;
+            break;
+        case '/':
+            answer = num1 / num2;
+            break;
+        default:
+            // oooops, not an operator...
+
+    }
+    console.log(answer);
+    history.push({
+        num1: num1,
+        num2: num2,
+        operator: operator,
+        answer: answer
+    })
+
+    console.log(history);
+    res.sendStatus(201);
+});
 
 app.listen(port, () => {
     console.log('listening on port, ', port);
 } ); // end of telling the application to listen to the port we defined above as 5001 and utilize node server we return
+
